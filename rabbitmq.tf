964baa8649b255fa33f3c3baa6aad7fc6a119860
Ravi <ravi@gmail.com>
2022-08-01 17:42:49 +0530

    ec2 code

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -3,8 +3,16 @@ resource "aws_spot_instance_request" "instance" {
   spot_price    = data.aws_ec2_spot_price.spot_price.spot_price
   instance_type = var.INSTANCE_TYPE
   wait_for_fulfillment = true
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id = var.PRIVATE_SUBNET_ID[0]
 
   tags = {
     Name = local.TAG_PREFIX
   }
+}
+
+resource "aws_ec2_tag" "example" {
+  resource_id = aws_spot_instance_request.instance.spot_instance_id
+  key         = "Name"
+  value       = local.TAG_PREFIX
 }
